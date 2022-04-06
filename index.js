a210c8e9a49aa949fbcbc9236e0ebed82b9568fb
caviarblue04 <103097906+caviarblue04@users.noreply.github.com>
2022-04-06 18:08:05 +0800

    code changes

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const app = express();
+const mongoose = require ('mongoose');
 
-
-//Import Routes
-const postsRoutes = require('./routes/posts');
-
-//Middleware
-app.use( '/posts', postsRoutes);
 
 const port = process.env.port || 8080; 
 app.listen(port, () => {
@@ -17,3 +12,65 @@ app.get('/', (req, res) => {
     res.send('Testing Homepage');
 });
 
+//Connect db
+mongoose.connect(
+    "mongodb+srv://dbAdmin:Abc123@cluster0.1um74.mongodb.net/Rest?retryWrites=true&w=majority", 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+app.get('/', async (req, res) => {
+    const post = new Post({
+        Status: res.body.Status,
+        SlotNo: res.body.SlotNo,
+        TradeNo: res.body.TradeNo
+    });
+    try{
+    const savedPost = await post.save();
+    res.json(savedPost);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+
+app.post('/', async (req, res) => {
+    const post = new Post({
+        Status: req.body.Status,
+        SlotNo: req.body.SlotNo,
+        TradeNo: req.body.TradeNo
+    });
+    try{
+    const savedPost = await post.save();
+    res.json(savedPost);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+
+const PostSchema = mongoose.Schema({
+    Status: {
+        type: Number,
+        require: true
+    },
+    SlotNo: {
+        type: String,
+        require: true
+    },
+    TradeNo: {
+        type: String,
+        require: true
+    },
+
+});
+
+module.exports = mongoose.model('Posts', PostSchema);
