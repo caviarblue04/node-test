088ba4596e508a1a6e55fcd89202de10bd8bb2ec
caviarblue04 <103097906+caviarblue04@users.noreply.github.com>
2022-04-06 17:46:16 +0800

    back to basic

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
-require ('dotenv/config');
 
-//Import Routes
-const postsRoutes = require('./routes/posts');
-
-//Middleware
-app.use( bodyparser.json());
-app.use( '/posts', postsRoutes);
 
 const port = process.env.port || 8080; 
 app.listen(port, () => {
@@ -20,18 +11,3 @@ app.get('/', (req, res) => {
     res.send('Testing Homepage');
 });
 
-
-//Connect db
-mongoose.connect(
-    process.env.DB_connection, 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-);
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
