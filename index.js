0314f447c81fec65806d70345cd24e2db4bf2138
caviarblue04 <103097906+caviarblue04@users.noreply.github.com>
2022-04-06 17:49:37 +0800

    restore setting

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+//Import Routes
+const postsRoutes = require('./routes/posts');
+
+//Middleware
+app.use( '/posts', postsRoutes);
 
 const port = process.env.port || 8080; 
 app.listen(port, () => {
@@ -11,3 +17,17 @@ app.get('/', (req, res) => {
     res.send('Testing Homepage');
 });
 
+//Connect db
+mongoose.connect(
+    "mongodb+srv://dbAdmin:Abc123@cluster0.1um74.mongodb.net/Rest?retryWrites=true&w=majority", 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
