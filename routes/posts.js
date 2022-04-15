f3c3f2b78ce97eef658e3d6d5c6260b5add33a0a
caviarblue04 <103097906+caviarblue04@users.noreply.github.com>
2022-04-15 16:34:51 +0800

    add if else

--- routes/posts.js
+++ routes/posts.js
@@ -20,15 +20,75 @@ router.post('/', async (req, res) => {
     var type = req.body.Type;
     var introduction = req.body.Introduction;
     var name = req.body.Name
+    var sessioncode = req.body.SessionCode;
+    var account = req.body.Account;
+    var pwd = req.body.PWD;
+    var imageurl = req.body.ImageUrl;
+    var MsgType = "0";
     var post = new Array({
-        funcode, machineid, tradeno, slotno, keynum, status, quantity, stock, capacity, productid, price, type, introduction, name
+        funcode, machineid, tradeno, slotno, keynum, status, quantity, stock, capacity, productid, price, type, introduction, name, pwd, account, sessioncode, imageurl
     });
     try{
     console.log(JSON.stringify(post))
-    res.send(({
-        Status: "0",
-        SlotNo: slotno,
-        TradeNo: tradeno}))
+
+    if (funcode === '1000'){
+        res.send({
+            Status: "0",
+            SlotNo: slotno,
+            TradeNo: tradeno,
+            Err:'Success'
+        })
+    }
+
+    else if (funcode === '2000'){
+        res.send(({
+            Status: "0",
+            SlotNo: slotno,
+            ProductID: productid,
+            TradeNo: tradeno,
+            Err:'Success'
+        }))
+    }
+
+    else if (funcode === "4000"){
+        if(MsgType === '0'){
+            res.send(({
+                Status: "0",
+                MsgType: "0",
+                TradeNo: tradeno,
+                SlotNo: slotno,
+                ProductID: productid,
+                Err:'Success'
+            }))
+        }
+        else if(MsgType === '1'){
+            res.send(({
+                Status: "0",
+                MsgType: "1",
+                SlotNo: slotno,
+                TradeNo: tradeno,
+                Capacity: capacity,
+                Quanitity: quantity,
+                ProductID: productid,
+                Name: name,
+                Price: price,
+                Type: type,
+                Introduction: introduction,
+                ImageUrl: imageurl,
+                ImageDetailUrl: "Image Details",
+                Err: "Success"
+            })) 
+        }
+    }
+
+    else if (funcode === '5000'){
+
+    }
+
+    else if (funcode === '5001'){
+
+    }
+
     }catch(err){
         res.send(({
             Status: "1",
